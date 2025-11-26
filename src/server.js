f7181088cc40632536ae67213370f5a793740f2e
yangelia <yana.sychova@gmail.com>
2025-11-26 18:21:32 +0200

    upd server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 
@@ -15,27 +14,26 @@ import { errors } from 'celebrate';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+// connect to DB BEFORE server start
 await connectMongoDB();
 
-// ----- GLOBAL MIDDLEWARE -----
 app.use(logger);
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-// ----- ROUTES -----
-app.use(notesRoutes);
+// ROUTES
+app.use('/notes', notesRoutes);
 
-// ----- 404 -----
+// 404
 app.use(notFoundHandler);
 
-// ----- CELEBRATE VALIDATION ERRORS -----
-app.use(errors()); // <-- ДОЛЖНО БЫТЬ ПЕРЕД errorHandler!!!
+// celebrate errors FIRST
+app.use(errors());
 
-// ----- GLOBAL ERROR HANDLER -----
+// global error handler LAST
 app.use(errorHandler);
 
-// ----- START SERVER -----
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
