a8ba198bbaf12b1bcdf2199ee0689427a15b9ba4
yangelia <yana.sychova@gmail.com>
2025-11-27 19:12:49 +0200

    upd authenticate

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -4,22 +4,25 @@ import { User } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
   try {
-    const { accessToken } = req.cookies || {};
+    const { accessToken } = req.cookies;
 
     if (!accessToken) {
       throw createHttpError(401, 'Missing access token');
     }
 
+    // Находим сессию
     const session = await Session.findOne({ accessToken });
 
     if (!session) {
       throw createHttpError(401, 'Session not found');
     }
 
+    // Проверяем срок действия
     if (session.accessTokenValidUntil < new Date()) {
       throw createHttpError(401, 'Access token expired');
     }
 
+    // Находим пользователя
     const user = await User.findById(session.userId);
 
     if (!user) {
@@ -27,7 +30,6 @@ export const authenticate = async (req, res, next) => {
     }
 
     req.user = user;
-
     next();
   } catch (err) {
     next(err);
