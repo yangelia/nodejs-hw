e72f9c25860213c3988cfb91d42b65ad7052c9e3
yangelia <yana.sychova@gmail.com>
2025-11-28 10:37:27 +0200

    temp

---
+++ src/utils/sendMail.js
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+/**
+ * options: { to, subject, html }
+ */
+export const sendEmail = async ({ to, subject, html }) => {
+  return await transporter.sendMail({
+    from: process.env.SMTP_FROM,
+    to,
+    subject,
+    html,
+  });
+};
