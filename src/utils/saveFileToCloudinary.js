5884106ea98be57bb0d43c770023ff897377f7af
yangelia <yana.sychova@gmail.com>
2025-12-06 16:48:35 +0200

    upd sendmail

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -14,6 +14,9 @@ export function saveFileToCloudinary(buffer) {
       {
         folder: 'students-app/avatars',
         resource_type: 'image',
+        overwrite: true,
+        unique_filename: true,
+        use_filename: false,
       },
       (err, result) => (err ? reject(err) : resolve(result)),
     );
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -9,11 +9,9 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendEmail = ({ to, subject, html }) => {
-  return transporter.sendMail({
-    from: process.env.SMTP_FROM,
-    to,
-    subject,
-    html,
+export const sendEmail = async (options) => {
+  return await transporter.sendMail({
+    from: options.from || process.env.SMTP_FROM,
+    ...options,
   });
 };
